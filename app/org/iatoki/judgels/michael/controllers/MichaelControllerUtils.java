03ad05db7df4d4ad6c090f49c1a7515b2f9f7d93
Jordan Fernando <fernandojordan.92@gmail.com>
2015-09-17 18:05:08 +0700

    Adapt to change in layouting system

--- app/org/iatoki/judgels/michael/controllers/MichaelControllerUtils.java
+++ app/org/iatoki/judgels/michael/controllers/MichaelControllerUtils.java
@@ -4,6 +4,7 @@ import com.google.common.collect.ImmutableList;
 import org.iatoki.judgels.play.InternalLink;
 import org.iatoki.judgels.play.LazyHtml;
 import org.iatoki.judgels.play.controllers.AbstractJudgelsControllerUtils;
+import org.iatoki.judgels.play.views.html.layouts.contentLayout;
 import org.iatoki.judgels.play.views.html.layouts.menusView;
 import org.iatoki.judgels.play.views.html.layouts.sidebarLayout;
 import play.i18n.Messages;
@@ -17,6 +18,8 @@ public final class MichaelControllerUtils extends AbstractJudgelsControllerUtils
 
     @Override
     public void appendSidebarLayout(LazyHtml content) {
+        content.appendLayout(c -> contentLayout.render(c));
+
         LazyHtml sidebarContent = new LazyHtml(menusView.render(ImmutableList.of(
               new InternalLink(Messages.get("application.applications"), routes.ApplicationController.index()),
               new InternalLink(Messages.get("dashboard.dashboards"), routes.DashboardController.index()),
