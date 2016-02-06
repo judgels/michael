819a51bb740689521b30b4a65e13307c43a00bb6
Ashar Fuadi <fushar@gmail.com>
2016-02-06 10:33:06 +0000

    Use correct base class for MichaelModule

--- app/org/iatoki/judgels/michael/MichaelModule.java
+++ app/org/iatoki/judgels/michael/MichaelModule.java
@@ -1,17 +1,17 @@
 package org.iatoki.judgels.michael;
 
+import com.google.inject.AbstractModule;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import org.iatoki.judgels.play.JudgelsPlayProperties;
-import org.iatoki.judgels.play.config.AbstractJudgelsPlayModule;
 import org.iatoki.judgels.play.general.GeneralName;
 import org.iatoki.judgels.play.general.GeneralVersion;
 import org.iatoki.judgels.play.migration.JudgelsDataMigrator;
 
-public class MichaelModule extends AbstractJudgelsPlayModule {
+public class MichaelModule extends AbstractModule {
 
     @Override
-    protected void manualBinding() {
+    public void configure() {
         org.iatoki.judgels.michael.BuildInfo$ buildInfo = org.iatoki.judgels.michael.BuildInfo$.MODULE$;
 
         bindConstant().annotatedWith(GeneralName.class).to(buildInfo.name());
@@ -24,15 +24,5 @@ public class MichaelModule extends AbstractJudgelsPlayModule {
         // </DEPRECATED>
 
         bind(JudgelsDataMigrator.class).to(MichaelDataMigrator.class);
-    }
-
-    @Override
-    protected String getDaosImplPackage() {
-        return "org.iatoki.judgels.michael.models.daos.impls";
-    }
-
-    @Override
-    protected String getServicesImplPackage() {
-        return "org.iatoki.judgels.michael.services.impls";
     }
 }
